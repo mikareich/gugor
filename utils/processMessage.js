21858f9a0d72b4680d8f578a00cf41e5dcf89040
Mika Reich <mikareich0@gmail.com>
2022-06-11 16:55:10 +0200

    remove waypoint init

--- utils/processMessage.js
+++ utils/processMessage.js
@@ -8,12 +8,6 @@ import { Message } from "discord.js";
 function processMessage(message) {
   if (!message.content.startsWith(botOptions.commandPrefix)) return;
   if (message.author.bot) return;
-
-  // yeah idk
-  message.channel.send("hey");
-
-  const waypoint = new Waypoint()
-
 }
 
 export default processMessage;
