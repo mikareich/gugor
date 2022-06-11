7ae0066c3a11666b6c0ee24602b2c335a422386c
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2022-06-11 16:07:22 +0200

    w

--- bot.js
+++ bot.js
@@ -1,21 +1,18 @@
 // Setup our environment variables via dotenv
 require("dotenv").config();
 // Import relevant classes from discord.js
-const { Client, Intents } = require("discord.js");
+import { Client, Intents } from "discord.js";
 // Instantiate a new client with some necessary parameters.
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 // Notify progress
-client.on("ready", function (e) {
+client.on("ready", (e) => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 // Authenticate
 client.login(process.env.DISCORD_TOKEN);
 
-//Example Functionality
-client.on("message", function (msg) {
-  if (msg.content === "Wer?") {
-    msg.reply("Der Bär!");
-  }
+client.on("message", (msg) => {
+  const msgParts = msg.content.split(" ");
 });
