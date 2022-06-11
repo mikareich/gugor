fad2e30ca5051d9f961ff12b4a5ed2cd1f58c5ba
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2022-06-11 15:24:03 +0200

    start

---
+++ waypointBot.js
@@ -0,0 +1,21 @@
+// Setup our environment variables via dotenv
+require("dotenv").config();
+// Import relevant classes from discord.js
+const { Client, Intents } = require("discord.js");
+// Instantiate a new client with some necessary parameters.
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+// Notify progress
+client.on("ready", function (e) {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+// Authenticate
+client.login(process.env.DISCORD_TOKEN);
+
+//Example Functionality
+client.on("message", function (msg) {
+  if (msg.content === "Wer?") {
+    msg.reply("Der Bär!");
+  }
+});
