1189fff4b5d2cfd974e252817d01dd8b85cb3cae
Mika Reich <mikareich0@gmail.com>
2022-06-15 20:00:02 +0200

    comment code

--- src/Discord/sendMessage.ts
+++ src/Discord/sendMessage.ts
@@ -1,20 +1,29 @@
-import { TextChannel } from "discord.js"
+import { Message, TextChannel } from "discord.js"
 import ErrorHandler from "../ErrorHandling/ErrorHandler"
 import botOptions from "../utils/botOptions"
 import channels from "../utils/channels"
 import client from "./client"
 
+/**
+ * It sends a message to a channel
+ * @param message - The message you want to send.
+ * @param channelID - The ID of the channel you want to send the message to.
+ * @returns A promise that resolves to a message or undefined
+ */
 async function sendMessage(
   message: string,
   channelID = channels[botOptions.defaultChannel]
-) {
+): Promise<Message | undefined> {
+  // get the channel
   const channel = (await client.channels.fetch(channelID)) as
     | TextChannel
     | undefined
 
-  if (!channel) return ErrorHandler.withCode(4) // channel not found
+  // if the channel is not found, return undefined
+  if (!channel) ErrorHandler.withCode(4, true)
 
-  return channel.send(message)
+  // send the message
+  return channel?.send(message)
 }
 
 export default sendMessage
