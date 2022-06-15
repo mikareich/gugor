dbf8e72bf92bee09910363f723a5ca33e0f52fec
Mika Reich <mikareich0@gmail.com>
2022-06-15 19:59:55 +0200

    emebd random joke api

--- src/CommandModules/Julia.ts
+++ src/CommandModules/Julia.ts
@@ -1,16 +1,32 @@
-import CommandModule from "./CommandModule"
+import sendMessage from "../Discord/sendMessage"
+import CommandModule, { CommandMap } from "./CommandModule"
 
 class Julia extends CommandModule {
-  static CommandMap = [
-    {
-      syntax: "",
-      description: "Easter egg",
-      execute: () => console.log("Easter egg"),
-    },
-  ]
-
   constructor() {
-    super("julia", Julia.CommandMap)
+    const commandMap: CommandMap = [
+      {
+        syntax: "",
+        description: "Easter egg",
+        execute: () => this.randomJoke(),
+      },
+    ]
+
+    super("julia", commandMap)
+  }
+
+  /**
+   * Fetches a random joke from dadjoke.io and sends it to the channel
+   */
+  private async randomJoke() {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+
+    const data = await response.json()
+
+    await sendMessage(data.joke)
   }
 }
 
